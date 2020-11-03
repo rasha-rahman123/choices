15ad4727e52619a3d4eef43796dfdc240260e7ec
Rasha Rahman <rasharahman@rashas-mbp.lan>
2020-11-03 15:09:44 -0800

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
 
 import '../styles/globals.css'
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return( <>
-
+<NextSeo
+      title="choices - decide ur fate"
+      description="When you cannot decide, let us take the wheel."
+      canonical="https://www.canonical.ie/"
+      openGraph={{
+        url: 'https://choices.vercel.app',
+        title: 'choices - decide ur fate',
+        description: 'When you cannot decide, let us take the wheel.',
+        images: [
+          {
+            url: 'https://i.imgur.com/vBLkXyd.png',
+            width: 800,
+            height: 600,
+            alt: 'Og Image Alt',
+          }
+        ],
+        site_name: 'choices',
+      }}
+      twitter={{
+        handle: '@raaahhh_sha',
+        site: '@raaahhh_sha',
+        cardType: 'summary_large_image',
+      }}
+    />
   <Component {...pageProps} />
   </>)
 }
